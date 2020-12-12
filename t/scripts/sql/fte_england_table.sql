109e038a41b1ea5e23e24c728972dd48ff8e7285
Stephen Condon <stcondon@gmail.com>
2020-12-12 14:48:13 -0800

    create england h&a inserts

--- t/scripts/sql/fte_england_table.sql
+++ t/scripts/sql/fte_england_table.sql
@@ -1,4 +1,36 @@
-WITH teams AS (
-  
-)
+INSERT INTO home
+  (team, played, points, scored, conceded)
+SELECT
+	team1 AS team,
+	COUNT (DISTINCT fte."index") AS played,
+	SUM ( CASE
+		WHEN score1 > score2 THEN 3
+		WHEN score1 = score2 THEN 1
+		ELSE 0
+	END) AS points,
+	SUM(score1) AS scored,
+	SUM(score2) AS conceded
+FROM fte.spi_matches fte
+INNER JOIN england_names en ON fte.team1 = en.fte
+INNER JOIN xpert.england xe ON TO_DATE(xe."Date",'DD/MM/YYYY') = DATE(fte."date") AND en.xpert = xe."HomeTeam"
+GROUP BY
+  1
+;
+INSERT INTO away
+  (team, played, points, scored, conceded)
+SELECT
+	team2 AS team,
+	COUNT (DISTINCT fte."index") AS played,
+	SUM ( CASE
+		WHEN score1 < score2 THEN 3
+		WHEN score1 = score2 THEN 1
+		ELSE 0
+	END) AS points,
+	SUM(score2) AS scored,
+	SUM(score1) AS conceded
+FROM fte.spi_matches fte
+INNER JOIN england_names en ON fte.team1 = en.fte
+INNER JOIN xpert.england xe ON TO_DATE(xe."Date",'DD/MM/YYYY') = DATE(fte."date") AND en.xpert = xe."HomeTeam"
+GROUP BY
+  1
 ;
