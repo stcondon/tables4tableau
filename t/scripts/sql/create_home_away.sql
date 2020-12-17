e1bdf0348ff5406638af9c945a9d4e9765db4355
Stephen Condon <stcondon@gmail.com>
2020-12-17 11:58:18 -0800

    a table and mini table per country

--- t/scripts/sql/create_home_away.sql
+++ t/scripts/sql/create_home_away.sql
@@ -1,4 +1,40 @@
-CREATE TABLE home(
+CREATE TABLE england_home(
+  id SERIAL PRIMARY KEY,
+  team TEXT,
+  played INTEGER,
+  points INTEGER,
+  scored INTEGER,
+  conceded INTEGER
+);
+
+CREATE TABLE england_away(
+  id SERIAL PRIMARY KEY,
+  team TEXT,
+  played INTEGER,
+  points INTEGER,
+  scored INTEGER,
+  conceded INTEGER
+);
+
+CREATE TABLE france_home(
+  id SERIAL PRIMARY KEY,
+  team TEXT,
+  played INTEGER,
+  points INTEGER,
+  scored INTEGER,
+  conceded INTEGER
+);
+
+CREATE TABLE france_away(
+  id SERIAL PRIMARY KEY,
+  team TEXT,
+  played INTEGER,
+  points INTEGER,
+  scored INTEGER,
+  conceded INTEGER
+);
+
+CREATE TABLE spain_home(
   id SERIAL PRIMARY KEY,
   team TEXT,
   played INTEGER,
@@ -8,7 +44,7 @@ CREATE TABLE home(
   tie_breaker INTEGER
 );
 
-CREATE TABLE away(
+CREATE TABLE spain_away(
   id SERIAL PRIMARY KEY,
   team TEXT,
   played INTEGER,
@@ -16,4 +52,93 @@ CREATE TABLE away(
   scored INTEGER,
   conceded INTEGER,
   tie_breaker INTEGER
+);
+CREATE TABLE spain_mini_home(
+  id SERIAL PRIMARY KEY,
+  team TEXT,
+  played INTEGER,
+  points INTEGER,
+  scored INTEGER,
+  conceded INTEGER
+);
+
+CREATE TABLE spain_mini_away(
+  id SERIAL PRIMARY KEY,
+  team TEXT,
+  played INTEGER,
+  points INTEGER,
+  scored INTEGER,
+  conceded INTEGER
+);
+CREATE TABLE germany_home(
+  id SERIAL PRIMARY KEY,
+  team TEXT,
+  played INTEGER,
+  points INTEGER,
+  scored INTEGER,
+  conceded INTEGER,
+  tie_breaker INTEGER
+);
+
+CREATE TABLE germany_away(
+  id SERIAL PRIMARY KEY,
+  team TEXT,
+  played INTEGER,
+  points INTEGER,
+  scored INTEGER,
+  conceded INTEGER,
+  tie_breaker INTEGER
+);
+CREATE TABLE germany_mini_home(
+  id SERIAL PRIMARY KEY,
+  team TEXT,
+  played INTEGER,
+  points INTEGER,
+  scored INTEGER,
+  conceded INTEGER
+);
+
+CREATE TABLE germany_mini_away(
+  id SERIAL PRIMARY KEY,
+  team TEXT,
+  played INTEGER,
+  points INTEGER,
+  scored INTEGER,
+  conceded INTEGER
+);
+CREATE TABLE italy_home(
+  id SERIAL PRIMARY KEY,
+  team TEXT,
+  played INTEGER,
+  points INTEGER,
+  scored INTEGER,
+  conceded INTEGER,
+  tie_breaker INTEGER
+);
+
+CREATE TABLE italy_away(
+  id SERIAL PRIMARY KEY,
+  team TEXT,
+  played INTEGER,
+  points INTEGER,
+  scored INTEGER,
+  conceded INTEGER,
+  tie_breaker INTEGER
+);
+CREATE TABLE italy_mini_home(
+  id SERIAL PRIMARY KEY,
+  team TEXT,
+  played INTEGER,
+  points INTEGER,
+  scored INTEGER,
+  conceded INTEGER
+);
+
+CREATE TABLE italy_mini_away(
+  id SERIAL PRIMARY KEY,
+  team TEXT,
+  played INTEGER,
+  points INTEGER,
+  scored INTEGER,
+  conceded INTEGER
 );
