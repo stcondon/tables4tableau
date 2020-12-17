334a71a7bba97c476ee5434885adbe0830edc47f
Stephen Condon <stcondon@gmail.com>
2020-12-17 12:01:57 -0800

    england home and away

--- t/scripts/sql/england_league_table_mv.sql
+++ t/scripts/sql/england_league_table_mv.sql
@@ -1,14 +1,14 @@
 CREATE MATERIALIZED VIEW england_league_table AS (
   SELECT
   	team,
-  	home.played + away.played AS played,
-  	home.points + away.points AS points,
-  	home.scored + away.scored AS scored,
-  	home.conceded + away.conceded AS conceded
-  FROM home
-  LEFT JOIN away USING (team)
+  	h.played + a.played AS played,
+  	h.points + a.points AS points,
+  	h.scored + a.scored AS scored,
+  	h.conceded + a.conceded AS conceded
+  FROM england_home
+  LEFT JOIN england_away USING (team)
   ORDER BY
-  	home.points + away.points DESC,
-  	home.scored + away.scored - home.conceded + away.conceded DESC,
-  	home.scored + away.scored DESC
+  	h.points + a.points DESC,
+  	h.scored + a.scored - h.conceded + a.conceded DESC,
+  	h.scored + a.scored DESC
 );
