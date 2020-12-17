86530b42742529f527bd0e9a5efe25483791746b
Stephen Condon <stcondon@gmail.com>
2020-12-17 12:01:00 -0800

    changed to england home and england away

--- t/scripts/sql/fte_england_table.sql
+++ t/scripts/sql/fte_england_table.sql
@@ -1,13 +1,14 @@
-INSERT INTO home
+INSERT INTO england_home
   (team, played, points, scored, conceded)
 SELECT
 	team1 AS team,
 	COUNT (DISTINCT fte."index") AS played,
-	SUM ( CASE
-		WHEN score1 > score2 THEN 3
-		WHEN score1 = score2 THEN 1
-		ELSE 0
-	END) AS points,
+	SUM (
+    CASE
+    	WHEN score1 > score2 THEN 3
+    	WHEN score1 = score2 THEN 1
+    	ELSE 0
+	  END) AS points,
 	SUM(score1) AS scored,
 	SUM(score2) AS conceded
 FROM fte.spi_matches fte
@@ -16,16 +17,18 @@ INNER JOIN xpert.england xe ON TO_DATE(xe."Date",'DD/MM/YYYY') = DATE(fte."date"
 GROUP BY
   1
 ;
-INSERT INTO away
+
+INSERT INTO england_away
   (team, played, points, scored, conceded)
 SELECT
 	team2 AS team,
 	COUNT (DISTINCT fte."index") AS played,
-	SUM ( CASE
-		WHEN score1 < score2 THEN 3
-		WHEN score1 = score2 THEN 1
-		ELSE 0
-	END) AS points,
+	SUM (
+    CASE
+    	WHEN score1 < score2 THEN 3
+    	WHEN score1 = score2 THEN 1
+    	ELSE 0
+	  END) AS points,
 	SUM(score2) AS scored,
 	SUM(score1) AS conceded
 FROM fte.spi_matches fte
@@ -34,3 +37,9 @@ INNER JOIN xpert.england xe ON TO_DATE(xe."Date",'DD/MM/YYYY') = DATE(fte."date"
 GROUP BY
   1
 ;
+
+REFRESH MATERIALIZED VIEW england_league_table;
+
+TRUNCATE TABLE england_home;
+
+TRUNCATE TABLE england_away;
