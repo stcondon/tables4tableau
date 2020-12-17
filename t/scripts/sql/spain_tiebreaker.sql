c94df7fd1dee70b44372a8b5072aaaa1b81a7dd9
Stephen Condon <stcondon@gmail.com>
2020-12-17 14:42:33 -0800

    added cte to improve aggregates

--- t/scripts/sql/spain_tiebreaker.sql
+++ t/scripts/sql/spain_tiebreaker.sql
@@ -15,11 +15,30 @@ BEGIN
     HAVING COUNT (DISTINCT team) > 1
   )
   LOOP
+    WITH mini AS (
+      SELECT
+        fte."index",
+        team1 AS team,
+      	score1,
+        score2
+      FROM fte.spi_matches fte
+      INNER JOIN xpert.spain xs ON TO_DATE(xs."Date",'DD/MM/YY') = DATE(fte."date")
+      INNER JOIN spain_league_table slt1 ON slt1.team = fte.team1
+      INNER JOIN spain_league_table slt2 ON slt2.team = fte.team2
+      WHERE 1 = 1
+        AND slt1.points = p.points
+  			AND slt2.points = p.points
+      GROUP BY
+        fte."index",
+        team1,
+        score1,
+        score2
+    )
     INSERT INTO spain_mini_home
       (team, played, points, scored, conceded)
     SELECT
-    	team1 AS team,
-    	COUNT (DISTINCT fte."index") AS played,
+    	team,
+    	COUNT (DISTINCT "index") AS played,
     	SUM (
         CASE
       		WHEN score1 > score2 THEN 3
@@ -28,13 +47,43 @@ BEGIN
     	  END) AS points,
     	SUM(score1) AS scored,
     	SUM(score2) AS conceded
-    FROM fte.spi_matches fte
-    INNER JOIN xpert.spain xs ON TO_DATE(xs."Date",'DD/MM/YY') = DATE(fte."date")
-    INNER JOIN spain_league_table slt1 ON slt1.team = fte.team1
-    INNER JOIN spain_league_table slt2 ON slt2.team = fte.team2
-    WHERE 1 = 1
-      AND slt1.points = p.points
-			AND slt2.points = p.points
+    FROM mini
+    GROUP BY
+      1
+    ;
+    WITH mini AS (
+      SELECT
+        fte."index",
+        team2 AS team,
+        score1,
+        score2
+      FROM fte.spi_matches fte
+      INNER JOIN xpert.spain xs ON TO_DATE(xs."Date",'DD/MM/YY') = DATE(fte."date")
+      INNER JOIN spain_league_table slt1 ON slt1.team = fte.team1
+      INNER JOIN spain_league_table slt2 ON slt2.team = fte.team2
+      WHERE 1 = 1
+        AND slt1.points = p.points
+        AND slt2.points = p.points
+      GROUP BY
+        fte."index",
+        team2,
+        score1,
+        score2
+    )
+    INSERT INTO spain_mini_away
+      (team, played, points, scored, conceded)
+    SELECT
+    	team,
+    	COUNT (DISTINCT "index") AS played,
+    	SUM (
+        CASE
+      		WHEN score1 < score2 THEN 3
+      		WHEN score1 = score2 THEN 1
+      		ELSE 0
+    	  END) AS points,
+    	SUM(score2) AS scored,
+    	SUM(score1) AS conceded
+    FROM mini
     GROUP BY
       1
     ;
