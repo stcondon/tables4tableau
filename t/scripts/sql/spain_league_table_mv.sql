b48363b655c0b4f4720eeb3ee48db0d6c52d93e5
Stephen Condon <stcondon@gmail.com>
2020-12-18 13:51:28 -0800

    just need the points and the difference and then to order the whole thing properly

--- t/scripts/sql/spain_league_table_mv.sql
+++ t/scripts/sql/spain_league_table_mv.sql
@@ -1,15 +1,25 @@
 DROP MATERIALIZED VIEW spain_league_table;
 CREATE MATERIALIZED VIEW spain_league_table AS (
   SELECT
-  	team,
+    team,
   	COALESCE(h.played, 0) + COALESCE(a.played, 0) AS played,
   	COALESCE(h.points, 0) + COALESCE(a.points, 0) AS points,
   	COALESCE(h.scored, 0) + COALESCE(a.scored, 0) AS scored,
-  	COALESCE(h.conceded, 0) + COALESCE(a.conceded, 0) AS conceded
-    , COALESCE(h.tie_breaker, 0) + COALESCE(a.tie_breaker, 0) as tb -- for debugging
+  	COALESCE(h.conceded, 0) + COALESCE(a.conceded, 0) AS conceded,
+    COALESCE(h.scored,0) + COALESCE(a.scored,0) - COALESCE(h.conceded,0) + COALESCE(a.conceded,0) AS difference,
+    RANK() OVER (
+      ORDER BY
+        COALESCE(h.points, 0) + COALESCE(a.points, 0) DESC,
+        COALESCE(h.tie_breaker, 0) + COALESCE(a.tie_breaker, 0),
+        COALESCE(h.scored,0) + COALESCE(a.scored,0) - COALESCE(h.conceded,0) + COALESCE(a.conceded,0) DESC,
+        COALESCE(h.scored,0) + COALESCE(a.scored,0) DESC
+    ) AS place
+    -- , COALESCE(h.tie_breaker, 0) + COALESCE(a.tie_breaker, 0) as tb -- for debugging
   FROM spain_home h
   LEFT JOIN spain_away a USING (team)
   ORDER BY
   	COALESCE(h.points, 0) + COALESCE(a.points, 0) DESC,
-    COALESCE(h.tie_breaker, 0) + COALESCE(a.tie_breaker, 0) DESC
+    COALESCE(h.tie_breaker, 0) + COALESCE(a.tie_breaker, 0),
+    COALESCE(h.scored,0) + COALESCE(a.scored,0) - COALESCE(h.conceded,0) + COALESCE(a.conceded,0) DESC,
+    COALESCE(h.scored,0) + COALESCE(a.scored,0)
 );
