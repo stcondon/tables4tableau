906747580eecf059b4f6bf70be056caaa81cbcad
Stephen Condon <stcondon@gmail.com>
2020-12-22 09:33:16 -0800

    added conditional, but gonna have to get it to initiate the view

--- t/scripts/sql/spain_tiebreaker.sql
+++ t/scripts/sql/spain_tiebreaker.sql
@@ -13,7 +13,6 @@ BEGIN
     WITH point_count AS (
     	SELECT
     		team,
-    		COALESCE(h.played, 0) + COALESCE(a.played, 0) AS played,
     		COALESCE(h.points, 0) + COALESCE(a.points, 0) AS points
     	FROM spain_home h
     	LEFT JOIN spain_away a USING (team)
@@ -25,7 +24,7 @@ BEGIN
     HAVING COUNT (DISTINCT team) > 1
   )
   LOOP
-    WITH mini AS (
+    WITH mini_home AS (
       SELECT
         fte."index",
         team1 AS team,
@@ -33,8 +32,10 @@ BEGIN
         score2
       FROM fte.spi_matches fte
       INNER JOIN xpert.spain xs ON TO_DATE(xs."Date",'DD/MM/YY') = DATE(fte."date")
-      INNER JOIN spain_league_table slt1 ON slt1.team = fte.team1
-      INNER JOIN spain_league_table slt2 ON slt2.team = fte.team2
+      INNER JOIN spain_names sn1 ON fte.team1 = sn1.fte
+      INNER JOIN spain_names sn2 ON fte.team1 = sn2.fte
+      -- INNER JOIN spain_league_table slt1 ON slt1.team = fte.team1
+      -- INNER JOIN spain_league_table slt2 ON slt2.team = fte.team2
       WHERE 1 = 1
         AND slt1.points = p.points
   			AND slt2.points = p.points
@@ -57,11 +58,11 @@ BEGIN
     	  END) AS points,
     	SUM(score1) AS scored,
     	SUM(score2) AS conceded
-    FROM mini
+    FROM mini_home
     GROUP BY
       1
     ;
-    WITH mini AS (
+    WITH mini_away AS (
       SELECT
         fte."index",
         team2 AS team,
@@ -69,8 +70,10 @@ BEGIN
         score2
       FROM fte.spi_matches fte
       INNER JOIN xpert.spain xs ON TO_DATE(xs."Date",'DD/MM/YY') = DATE(fte."date")
-      INNER JOIN spain_league_table slt1 ON slt1.team = fte.team1
-      INNER JOIN spain_league_table slt2 ON slt2.team = fte.team2
+      INNER JOIN spain_names sn1 ON fte.team1 = sn1.fte
+      INNER JOIN spain_names sn2 ON fte.team1 = sn2.fte
+      -- INNER JOIN spain_league_table slt1 ON slt1.team = fte.team1
+      -- INNER JOIN spain_league_table slt2 ON slt2.team = fte.team2
       WHERE 1 = 1
         AND slt1.points = p.points
         AND slt2.points = p.points
@@ -93,35 +96,51 @@ BEGIN
     	  END) AS points,
     	SUM(score2) AS scored,
     	SUM(score1) AS conceded
-    FROM mini
+    FROM mini_away
     GROUP BY
       1
     ;
-    WITH tb AS (
-      SELECT
-        team,
-        COALESCE(h.played, 0) + COALESCE(a.played, 0) AS played,
-        COALESCE(h.points, 0) + COALESCE(a.points, 0) AS points,
-        COALESCE(h.scored,0) + COALESCE(a.scored,0) - COALESCE(h.conceded,0) + COALESCE(a.conceded,0) AS difference,
-        RANK() OVER (
-          ORDER BY
+    WITH mini_teams AS (
+      SELECT team FROM spain_mini_home
+      UNION
+      SELECT team FROM spain_mini_away
+    )
+    SELECT COUNT (DISTINCT team) INTO team_count FROM mini_teams;
+    WITH mini_games AS (
+      SELECT played FROM spain_mini_away
+      UNION
+      SELECT played FROM spain_mini_home
+    )
+    SELECT SUM(played) INTO game_count FROM mini_games
+    ;
+    IF game_count = 2 * (team_count * (team_count - 1)) THEN
+      WITH tb AS (
+        SELECT
+          team,
+          COALESCE(h.played, 0) + COALESCE(a.played, 0) AS played,
+          COALESCE(h.points, 0) + COALESCE(a.points, 0) AS points,
+          COALESCE(h.scored,0) + COALESCE(a.scored,0) - COALESCE(h.conceded,0) + COALESCE(a.conceded,0) AS difference,
+          RANK() OVER (
+            ORDER BY
+            COALESCE(h.points, 0) + COALESCE(a.points, 0) DESC,
+            COALESCE(h.scored,0) + COALESCE(a.scored,0) - COALESCE(h.conceded,0) + COALESCE(a.conceded,0) DESC
+          ) AS place
+        FROM spain_mini_home h
+        LEFT JOIN spain_mini_away a USING (team)
+        GROUP BY
+          1,2,3,4
+        ORDER BY
           COALESCE(h.points, 0) + COALESCE(a.points, 0) DESC,
           COALESCE(h.scored,0) + COALESCE(a.scored,0) - COALESCE(h.conceded,0) + COALESCE(a.conceded,0) DESC
-        ) AS place
-      FROM spain_mini_home h
-      LEFT JOIN spain_mini_away a USING (team)
-      GROUP BY
-        1,2,3,4
-      ORDER BY
-        COALESCE(h.points, 0) + COALESCE(a.points, 0) DESC,
-        COALESCE(h.scored,0) + COALESCE(a.scored,0) - COALESCE(h.conceded,0) + COALESCE(a.conceded,0) DESC
-    )
-    UPDATE spain_home sh
-      SET tie_breaker = tb.place
-    FROM tb
-    WHERE sh.team = tb.team
-    ;
+      )
+      UPDATE spain_home sh
+        SET tie_breaker = tb.place
+      FROM tb
+      WHERE sh.team = tb.team
+      ;
+    END IF;
     TRUNCATE spain_mini_home;
+    TRUNCATE spain_mini_away;
   END LOOP;
   REFRESH MATERIALIZED VIEW spain_league_table;
   RETURN NULL;
